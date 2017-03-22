4585b47b0e6a445138931826d863a17d42250a33
Daniel Winder <daniel.winder@hotmail.com>
2017-03-22 19:15:28 +0100

    erneuerung

--- uebungifelse/uebungifelse/Program.cs
+++ uebungifelse/uebungifelse/Program.cs
@@ -8,22 +8,32 @@ namespace uebungifelse
 {
     class Program
     {
-       
-       static int highscore = 10000;
+
+        static int highscorePlayer2 = 15000;
+        
+        static string player1Name = "";
+        static string player2Name = "DanielTheBeast";
 
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Wie lautet ihr name Player 1 ?");
+            string player1name = Console.ReadLine();
+
+
             Console.WriteLine("Wie hoch ist deine Punktezahl?");
-            int score = Convert.ToInt32(Console.ReadLine());
-        
-            if  (score >= highscore)
+            int scorePlayer1 = Convert.ToInt32(Console.ReadLine());
+
+            if (scorePlayer1 >= highscorePlayer2)
             {
-                Console.WriteLine("Du hast den Highscore gecknackt");
-            
+                Console.WriteLine("Du hast den Highscore gecknackt" + player2Name + "Ist nun auf rang 2");
+
             }
             else
             {
-                Console.WriteLine("Leider hast du den Highscóre nicht gecknackt");
+                Console.WriteLine("Leider hast du den Highscóre nicht gecknackt. Highscore von" +  player2Name + highscorePlayer2);
+                Console.WriteLine( player1name +  "Versuche es Erneurt vlt ein anderes mal");
+
             }
 
             Console.ReadKey();
